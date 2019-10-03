71ddebcbab4d1c38f9e214218b171d0a41c77477
Michael Potanin <potanin.michael@gmail.com>
2019-10-03 15:10:50 +0300

    add ProductReaders

---
+++ ProductReaders/MACCSMetadata/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /sen2agri/distrib/Sen2Agri-System-master/sen2agri-processors/ProductReaders/MACCSMetadata/src
+# Build directory: /sen2agri/distrib/Sen2Agri-System-master/sen2agri-processors/ProductReaders/MACCSMetadata/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
