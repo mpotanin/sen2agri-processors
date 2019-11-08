de7cc23e40c4c506d3d352843483ad1457bd810e
Michael Potanin <potanin.michael@gmail.com>
2019-11-08 17:31:52 +0300

    add TrainImagesClassifierNew processor with updates:
    - comment out method body TrainImagesClassifier::TrainGradientBoostedTree
    - comment out SetDocName
    - replace ParameterType_Empty -> ParameterType_Bool
    - replace TrainImagesClassifier -> TrainImagesClassifierNew
    -replace MachineLearningModel::PredictAll() -> MachineLearningModel::PredictBatch()

---
+++ Common/TrainImagesClassifierNew/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /sen2agri/distrib/Sen2Agri-System-master/sen2agri-processors/Common/TrainImagesClassifierNew
+# Build directory: /sen2agri/distrib/Sen2Agri-System-master/sen2agri-processors/Common/TrainImagesClassifierNew
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("test")
